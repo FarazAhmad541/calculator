75e75e59266561789713755fd001aeac91f17a5f
Faraz <63188075+FarazAhmad541@users.noreply.github.com>
2024-09-16 22:50:50 +0500

    Added Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,68 @@
+import clsx from 'clsx'
+import { useReducer } from 'react'
 import './App.css'
-import Buttons from './components/buttons'
+import { buttons } from './components/buttons'
+
+const initial_state = {
+  current_value: '',
+  prev_value: '',
+  operation: null,
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD DIGIT': {
+      return {
+        ...state,
+        current_value: state.current_value + action.payload,
+      }
+    }
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initial_state)
+
+  const handleButtonClick = (button) => {
+    if (button === 'AC') {
+      dispatch({ type: 'CLEAR' })
+    } else if (button === '=') {
+      dispatch({ type: 'EVALUATE' })
+    } else if (button === '+/-') {
+      dispatch({ type: 'TOGGLE SIGN' })
+    } else if (button === '.') {
+      dispatch({ type: 'INSERT DECIMAL' })
+    } else if (['+', '-', '×', '÷', '%'].includes(button)) {
+      dispatch({ type: 'OPERATION', payload: button })
+    } else {
+      dispatch({ type: 'ADD DIGIT', payload: button })
+    }
+  }
   return (
     <main className='w-3/4 md:w-3/6 h-20'>
       <input
         type='text'
         className='bg-slate-900 text-white p-7 text-2xl w-full text-right'
+        readOnly
+        value={state.current_value}
       />
       <div className='grid grid-cols-4 border-collapse '>
-        <Buttons />
+        {buttons.map((button, index) => {
+          return (
+            <button
+              key={index}
+              className={clsx(
+                'p-7 text-4xl border border-slate-400 border-collapse hover:bg-slate-200',
+                ['+', '-', '=', '÷', '×'].includes(button) &&
+                  'bg-orange-400 text-white hover:bg-orange-500',
+                button == '0' && 'col-span-2'
+              )}
+              onClick={() => handleButtonClick(button)}
+            >
+              {button}
+            </button>
+          )
+        })}
       </div>
     </main>
   )
---
+++ src/components/buttons.js
@@ -0,0 +1,43 @@
+export const buttons = [
+  'AC',
+  '+/-',
+  '%',
+  '÷',
+  '7',
+  '8',
+  '9',
+  '×',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '0',
+  '.',
+  '=',
+]
+
+// export default function Buttons() {
+//   return (
+//     <>
+//       {buttons.map((button, index) => {
+//         return (
+//           <button
+//             key={index}
+//             className={clsx(
+//               'p-7 text-4xl border border-slate-400 border-collapse hover:bg-slate-200',
+//               ['+', '-', '=', '÷', '×'].includes(button) &&
+//                 'bg-orange-400 text-white hover:bg-orange-500',
+//               button == '0' && 'col-span-2'
+//             )}
+//           >
+//             {button}
+//           </button>
+//         )
+//       })}
+//     </>
+//   )
+// }
